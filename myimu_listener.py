d32d5b87c4a5c8d43eef16b3dc56085f58c1eabe
orhanoz <orhanoz91@gmail.com>
2016-07-11 13:03:15 +0100

    Add files via upload

---
+++ myimu_listener.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+'''
+this shows how you can take imu data from /mobile_base/sensors/imu_data
+the data is logged as quaternion , linear accel , angular velocity
+'''
+
+import rospy
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Quaternion , Vector3
+
+def callback(data):
+	rospy.loginfo('Quaternion data:[%f ,%f ,%f ,%f]'%(data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w))
+	rospy.loginfo('Linear acceleration:[%f ,%f ,%f]'%(data.linear_acceleration.x,data.linear_acceleration.y,data.linear_acceleration.z))
+	rospy.loginfo('Angular velocity:[%f ,%f ,%f]'%(data.angular_velocity.x,data.angular_velocity.y,data.angular_velocity.z))
+	
+	
+def listener():
+	rospy.init_node('myimu_listener')
+	rospy.Subscriber('/mobile_base/sensors/imu_data',Imu,callback)
+	rospy.spin()
+	
+if __name__ == '__main__':
+	listener()
