81174e1ef894e57febd6c70b00aed6bddd221ccb
orhanoz <orhanoz91@gmail.com>
2016-07-11 14:31:15 +0100

    Update myimu_listener.py

--- myimu_listener.py
+++ myimu_listener.py
@@ -10,6 +10,8 @@ from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion , Vector3
 
 def callback(data):
+	rospy.loginfo('HeaderID: %s'%data.header.frame_id)
+	rospy.loginfo('Timestamp: %s'%data.header.stamp)
 	rospy.loginfo('Quaternion data:[%f ,%f ,%f ,%f]'%(data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w))
 	rospy.loginfo('Linear acceleration:[%f ,%f ,%f]'%(data.linear_acceleration.x,data.linear_acceleration.y,data.linear_acceleration.z))
 	rospy.loginfo('Angular velocity:[%f ,%f ,%f]'%(data.angular_velocity.x,data.angular_velocity.y,data.angular_velocity.z))
